b64875016131ddaea04c8abadbb77e4306e69d02
Davi Damião <daviddperera10@gmail.com>
2024-07-11 13:04:25 +0100

    added an error handling

--- frontend/src/components/useQuery/useQuery.tsx
+++ frontend/src/components/useQuery/useQuery.tsx
@@ -10,16 +10,27 @@ type DataType =
 
 export default function UseQuery({ location }: { location: string }) {
   const fetchData = async () => {
-    const data: DataType = await fetch(
-      `${import.meta.env.VITE_API_URL}?city=${location}`
-    ).then((res) => res.json());
-    return data;
+    if (!location) return;
+    try {
+      const data: DataType = await fetch(
+        `${import.meta.env.VITE_API_URL}?city=${location}`
+      ).then((res) => res.json());
+
+      return data;
+    } catch (error) {
+      const data = {
+        error: true,
+        message: "We encountered an error while trying to retrieve the data.",
+      } as DataType;
+      return data;
+    }
   };
   const { data, refetch, isLoading } = useQuery({
     queryKey: [location],
     queryFn: fetchData,
     enabled: false,
   });
+
   return {
     data,
     refetch: () => refetch(),
